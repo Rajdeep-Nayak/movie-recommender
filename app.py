759f15cce9d59ca763ec371b4e5a24a81262796a
Rajdeep-Nayak <161142356+Rajdeep-Nayak@users.noreply.github.com>
2025-10-04 13:07:44 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,29 @@
+import pickle
+import streamlit as st
+import pandas as pd
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
+    distances = similarity[movie_index]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    for i in movies_list:
+        recommended_movies.append(movies.iloc[i[0]].title)
+    return recommended_movies
+
+movies_dict = pickle.load(open('movie_dictt.pkl', 'rb'))
+movies = pd.DataFrame(movies_dict)
+
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+st.title('Movie Recommender System')
+
+selected_movie_name = st.selectbox(
+    'Select a movie you like, and we will find similar ones for you.',
+    movies['title'].values)
+
+if st.button('Recommend'):
+    recommendations = recommend(selected_movie_name)
+    for movie_title in recommendations:
+        st.write(movie_title)
